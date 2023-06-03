4c49dec8385f24b15124a9de66277a3a3327cd0a
Zach Leone <zjleone@gmail.com>
2023-06-03 12:50:32 -0700

    fixed formatting and added some styling to generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,43 +8,62 @@ const Intern = require('../lib/Intern');
 // Function that generates the HTML file
 const generateHTML = (answers) => {
 
-  // Map over the answers array to create the employee cards depending on the role and create template literals for each role
-  const employeeCards = answers.map((employee) => {
-    let properties = '';
-    if (employee instanceof Manager) {
-      properties = `Office Number: ${employee.officeNumber}`;
-    } else if (employee instanceof Engineer) {
-      properties = `GitHub: ${employee.github}`;
-    } else if (employee instanceof Intern) {
-      properties = `School: ${employee.school}`;
-    }
+    // Map over the answers array to create the employee cards depending on the role and create template literals for each role
+    const employeeCards = answers.map((employee) => {
+        let properties = '';
+        if (employee instanceof Manager) {
+            properties = `Office Number: ${employee.officeNumber}`;
+        } else if (employee instanceof Engineer) {
+            properties = `GitHub: ${employee.github}`;
+        } else if (employee instanceof Intern) {
+            properties = `School: ${employee.school}`;
+        }
 
-    return `
-      <div class="col-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${employee.name}</h5>
-            <p class="card-text">ID: ${employee.id}</p>
-            <p class="card-text">Email: ${employee.email}</p>
-            <p class="card-text">${properties}</p>
-          </div>
-        </div>
-      </div>
+        return `
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title employee-name">${employee.name} ${employee.getRole()}</h5>
+                    <p class="card-text">ID: ${employee.id}</p>
+                    <p class="card-text">Email: ${employee.email}</p>
+                    <p class="card-text">${properties}</p>
+                </div>
+            </div>
+         </div>
+  
     `;
-  });
+    });
 
-  // Return the HTML template literal with the employee cards
-  return `<!DOCTYPE html>
+    // Return the HTML template literal with the employee cards
+    return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Team Profile Generator</title>
+
+        <!-- Custom Styling I added-->
+        <style>
+          .banner {
+            background-color: red;
+            padding: 20px;
+            text-align: center;
+            color: white;
+          }
+          .employee-name {
+            background-color: blue;
+            color: white;
+            padding: 5px;
+          }
+        </style>
+
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     </head>
+
+
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 banner">
                     <h1 class="text-center">My Team</h1>
                 </div>
             </div>
